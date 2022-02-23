9fdb17f10f766562e946740c4d3f3ad7c2e2fddc
huypq <huyphamquoc@admicro.vn>
2022-02-23 12:05:47 +0700

    sigmoid heblock

--- network/common/he_block.py
+++ network/common/he_block.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 
 
-class HEBlock(nn.Module):
+class HEMaxBlock(nn.Module):
     def __init__(self, beta):
-        super().__init__()
+        super(HEMaxBlock).__init__()
         self.beta = beta
     
     def forward(self, X):
@@ -14,5 +14,21 @@ class HEBlock(nn.Module):
         shape = X.shape
         max_value, _ = torch.max(X.view(*shape[:2], -1), dim = -1)
         mask = X == max_value[:, :, None, None]
+        X[mask] *= self.beta
+        return X
+
+class HESigmoidBlock(nn.Module):
+    def __init__(self, threshold, beta):
+        super(HESigmoidBlock, self).__init__()
+        self.threshold = threshold
+        self.beta = beta
+
+    def forward(self, X):
+        '''
+            X: (batch_size, channel, w, h)
+        '''
+        shape = X.shape
+        act_value = torch.sigmoid(X)
+        mask = X == act_value > self.threshold
         X[mask] *= self.beta
         return X
