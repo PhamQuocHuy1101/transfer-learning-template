85d562112232c9c19debfbcd79c6ac7a8b6044e8
huypq <huyphamquoc@admicro.vn>
2022-02-23 09:43:04 +0700

    HENet

--- network/common/__init__.py
+++ network/common/__init__.py
@@ -1,3 +1,4 @@
 from .stem import Stem
 from .inception_block import Inception_A, Inception_B, Inception_C
 from .reduction_block import Reduction_A, Reduction_B
+from .he_block import HEBlock
--- network/henet.py
+++ network/henet.py
@@ -1,16 +1,26 @@
+from numpy import block
 import torch
 import torch.nn as nn
 from torchvision import models
-
+from common import HEBlock
 
 class HENet(nn.Module):
-    def __init__(self, n_class):
+    def __init__(self, n_class, beta, block_expansion = 1):
         super().__init__()
         n_features = 512
         backbone = models.resnet18(pretrained=True)
         self.backbone = nn.Sequential(*list(backbone.children())[:-2])
-        self.reduce_channel = nn.Conv2d(n_features, n_class, 1, 1)
-        # self.mask = torch
+        self.HE_block = HEBlock(n_features, n_class * block_expansion, beta)
+        self.pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(n_class * block_expansion, n_class)
+        
     def forward(self, X):
-        feature_map = self.reduce(self.backbone(X))
-        mask = torch.
+        '''
+            X: (batch_size, channel, w, h)
+        '''
+        feature_map = self.backbone(X)
+        feature_map = self.HE_block(feature_map)
+        out = self.pool(feature_map)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        return out
